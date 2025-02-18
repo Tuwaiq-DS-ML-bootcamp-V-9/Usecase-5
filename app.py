f406a5637e694e79050120e0e39a20c07e9d0478
MushalMohammed1 <mohd.1mohd.123@gmail.com>
2025-02-18 20:51:41 +0300

    Mushal Alshagha

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.markdown('<h2 style="text-align: right; direction: rtl;">الحمدلله حصلت على الرخصة اليوم 🥳🎉</h2>', unsafe_allow_html=True)
+st.image("driver-license-icon-in-flat-style-identification-document-illustration-on-isolated-background-profile-card-sign-business-concept-vector.jpg", width = 650)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">لكن واجهت مشكلة وش اول سيارة اشتريها  ما ابي وحده جديدة ابي مستعمل كبدايه لقيت موقع سيارة يبيع سيارات مستعملة بس لسه ماعرفت اي سيارة اشتريها الخيارات كثيرة!
+فخلوني اخذكم معي بأبرز الاشياء اللي ساعدتني اختيار السيارة المنشوده.</h3>''', unsafe_allow_html=True)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">اول شي قلت ابي اعرف ايش ارخص السيارات المعروضه ؟ شفت اول ارخص عشره بس ماكانوا خيارات جيدة </h3>''', unsafe_allow_html=True)
+st.image("ch1.png", width = 750)
+st.markdown('''<h3 style="text-align: right; direction: rtl;">ويوم بحثت عن صور السيارات طلعت لي هذي!!</h3>''', unsafe_allow_html=True)
+st.image("oldCar.jpg", width = 400)
+st.markdown('''<h3 style="text-align: right; direction: rtl;"> تصيح صح 🥹</h3>''', unsafe_allow_html=True)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">بعدها قررت أحط لنفسي سعر يناسب ميزانيتي 
+وبديت أدور سيارات سعرها بين عشرين الف الى ثلاثين الف وتكون اقتصادية بنفس الوقت!</h3>''', unsafe_allow_html=True)
+st.image("ch2.png", width = 750)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">طلع لي خيارات جيدة بحدود هذا السعر حلو الحين أقدر أختار سيارتي  </h3>''', unsafe_allow_html=True)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">بس لحظة ايش نوع القير؟ أكيد ما أبغى قير عادي 😧🙂</h3>''', unsafe_allow_html=True)
+st.image("ch3.png", width = 750)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">ايه الحين اقدر اختار</h3>''', unsafe_allow_html=True)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">واخيرا استعملت مهاراتي في الداتا ساينس اللي سهلت علي عملية البحث عن السيارة اللي تناسبني وصارت الخيارات تناسب المعايير اللي أبيها 
+</h3>''', unsafe_allow_html=True)
+st.markdown('''<h3 style="text-align: right; direction: rtl;">بحدود ميزانيتي ✔️</h3>''', unsafe_allow_html=True)
+st.markdown('''<h3 style="text-align: right; direction: rtl;">اقتصادية ✔️</h3>''', unsafe_allow_html=True)
+st.markdown('''<h3 style="text-align: right; direction: rtl;">قير أوتاميتك ✔️</h3>''', unsafe_allow_html=True)
+st.write('')
+st.markdown('''<h3 style="text-align: right; direction: rtl;">و وقع الاختيار على هذي المزيونة 😍</h3>''', unsafe_allow_html=True)
+st.image("newCar.jpeg", width = 600)
