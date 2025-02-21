5840d2849f646d202052f2253795d625cc2d114a
MushalMohammed1 <125604010+MushalMohammed1@users.noreply.github.com>
2025-02-21 15:38:08 +0300

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_css(theme):
         /* Hero Section Styling */
         .hero {{
             background: linear-gradient({theme['hero_overlay']}, {theme['hero_overlay']}), 
-                        url('https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf') center/cover;
+                        url('https://unsplash.com/photos/hallway-between-glass-panel-doors-yWwob8kwOCk') center/cover;
             padding: 4rem 2rem;
             border-radius: 30px;
             margin: 2rem 0;
