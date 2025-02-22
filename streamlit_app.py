04f02353f661ceb1890df029667db5bf95355fa4
Tahani Alotaibi <79321400+TMQ5@users.noreply.github.com>
2025-02-22 14:00:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,28 +189,30 @@ def main():
         return
 
 
-        # تصفية البيانات لحديثي التخرج
-        df_fresh = jadarat[jadarat['experience_categories'] == " خريجون جدد"]
+    # تصفية البيانات لحديثي التخرج
+    df_fresh = jadarat[jadarat['experience_categories'] == " خريجون جدد"]
         
-        # حساب متوسط الراتب لحديثي التخرج حسب الوظيفة
+        
+       
+    # حساب متوسط الراتب لحديثي التخرج حسب الوظيفة
         avg_salary_by_job = df_fresh.groupby('job_title')['salary'].mean()
         
-        # ترتيب الوظائف من الأعلى إلى الأدنى
+    # ترتيب الوظائف من الأعلى إلى الأدنى
         avg_salary_by_job_sorted = avg_salary_by_job.sort_values(ascending=False)
         
-        # اختيار أعلى 10 وظائف
-        top_10_jobs = avg_salary_by_job_sorted.head(10)
+    # اختيار أعلى 10 وظائف
+    top_10_jobs = avg_salary_by_job_sorted.head(10)
         
-        # رسم المخطط الشريطي لأعلى 10 وظائف فقط
-        st.subheader("أعلى 10 وظائف لحديثي التخرج من حيث متوسط الراتب")
-        plt.figure(figsize=(10, 5))
-        sns.barplot(x=top_10_jobs.index, y=top_10_jobs.values, palette="viridis")
-        plt.xticks(rotation=45)
-        plt.xlabel("المسمى الوظيفي", fontsize=12)
-        plt.ylabel("متوسط الراتب", fontsize=12)
-        plt.title("أعلى 10 وظائف لحديثي التخرج من حيث متوسط الراتب", fontsize=14)
-        st.pyplot(plt.gcf())
-        plt.clf()
+    # رسم المخطط الشريطي لأعلى 10 وظائف فقط
+    st.subheader("أعلى 10 وظائف لحديثي التخرج من حيث متوسط الراتب")
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=top_10_jobs.index, y=top_10_jobs.values, palette="viridis")
+    plt.xticks(rotation=45)
+    plt.xlabel("المسمى الوظيفي", fontsize=12)
+    plt.ylabel("متوسط الراتب", fontsize=12)
+    plt.title("أعلى 10 وظائف لحديثي التخرج من حيث متوسط الراتب", fontsize=14)
+    st.pyplot(plt.gcf())
+    plt.clf()
 
 
 
