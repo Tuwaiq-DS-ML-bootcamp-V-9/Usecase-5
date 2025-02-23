542359681077e51e4c90d60d8cbbc07f106dad43
kawthar-mohammed <Kawtharmoh039@gmail.com>
2025-02-23 23:37:15 +0300

    Update Jadarat.py

--- Jadarat.py
+++ Jadarat.py
@@ -9,28 +9,11 @@ Jadarat_data = pd.read_csv("cleaned_Jadarat_data")  # Example: loading from CSV
 
 # --- Streamlit App ---
 st.title("Jadarat Data Visualization")
-unique_regions = Jadarat_data['region'].unique()
-region_counts = Jadarat_data['region'].value_counts()
+fig_1 = st.file_uploader('fig_1',type = 'png')
 
-# Reshape Arabic text
-reshaped_regions = []  # Initialize an empty list
-for item in unique_regions:
-     reshaped_item = get_display(arabic_reshaper.reshape(item))
-     reshaped_regions.append(reshaped_item)
-         
-# Create Plotly chart
-fig = go.Figure(data=[go.Bar(x=reshaped_regions, y=region_counts)])
-# Update layout
-fig.update_layout(
-    xaxis_title=get_display(arabic_reshaper.reshape("المناطق")),
-    yaxis_title=get_display(arabic_reshaper.reshape("عدد الوظائف المتاحة")),
-    title=get_display(arabic_reshaper.reshape("الوظائف المعروضة حسب المناطق")),
-    font=dict(family='Arial Unicode MS', size=12),
-    xaxis=dict(tickangle=45),
-    title_x=0.5
-    )
-
-# Show the Plotly chart in Streamlit
-st.plotly_chart(fig)
-
-
+if fig_1 is not None:
+    try:
+        image = Image.open(uploaded_file) # Open the uploaded file using PIL
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+    except Exception as e:
+        st.error(f"Error processing uploaded image: {e}")
