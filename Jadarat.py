ed0e9b03f92f29a40a2c975efe7672ca8bad8e3a
kawthar-mohammed <Kawtharmoh039@gmail.com>
2025-02-23 23:54:27 +0300

    Update Jadarat.py

--- Jadarat.py
+++ Jadarat.py
@@ -9,13 +9,13 @@ fig_4 = st.file_uploader('fig_4',type = 'png')
 
 if fig_1 is not None:
     try:
-        image = Image.open(fig_1) # Open the uploaded file using PIL
+        
         st.image(fig_1, caption="Uploaded Image", use_column_width=True)
-        image = Image.open(fig_2)
+    
         st.image(fig_2, caption="Uploaded Image", use_column_width=True)
-        image = Image.open(fig_3)
+       
         st.image(fig_3, caption="Uploaded Image", use_column_width=True)
-        image = Image.open(fig_4)
+       
         st.image(fig_4, caption="Uploaded Image", use_column_width=True)
     except Exception as e:
         st.error(f"Error processing uploaded image: {e}")
